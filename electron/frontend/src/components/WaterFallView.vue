4e966ebd04df6f9d29bfde72671f71f333fdfb85
abishanka <abishanka@gmail.com>
2024-02-23 02:42:46 -0500

    Completed Sidebar

--- electron/frontend/src/components/WaterFallView.vue
+++ electron/frontend/src/components/WaterFallView.vue
@@ -1,15 +1,42 @@
 <template>
-    <div>
-        
+    <div class="main-container">
+        <div class="sidebar container" style="margin-left: 0; margin-right: 0;">
+            <div class="d-flex align-items-center">
+                <div class="logo">
+                <img src="../assets/images/logo.png" alt="Logo"> 
+                </div>
+                <div class="headername">STRIDE</div>
+            </div>
+
+            <div class="container text-center sidebar-menu">
+                <div class="row sidebar-options" v-for="option in sidebaroptions" :key="option" @click="optionClick(option)">
+                    <div class="col options">{{option}}</div>
+                </div>
+            </div>
+
+            <div class="container mt-auto position-absolute bottom-0 homebutton">
+                Return to Home
+            </div>
+
+        </div>
+        <div>
+         
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'WaterFallView'
+    name: 'WaterFallView',
+    data() {
+    return {
+      sidebaroptions: ['Waterfall', 'Cadet Profile', 'Export'],
+    }
+  },
 }
   
 import '../assets/styles/WaterFallView.css';
+import '../assets/styles/Sidebar.css';
 </script>
 
 <style scoped>
