10de81221073add9be86cda33db94821d67d0422
jorsab121 <sabo.jordan121@gmail.com>
2024-02-23 14:05:59 -0500

    connect electron to database

--- electron/main.js
+++ electron/main.js
@@ -1,29 +1,41 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
+const sqlite3 = require("sqlite3");
 
-function createWindow () {
+const DB_PATH = "database/stride.db";
+
+//connect to db
+const db = new sqlite3.Database(DB_PATH, sqlite3.OPEN_READONLY, (err) => {
+  if (err) {
+    console.error("Error opening database:", err.message);
+  } else {
+    console.log("Connected to the SQLite database.");
+  }
+});
+
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
-  })
+      nodeIntegration: true,
+    },
+  });
 
   // Load the Vue app
   // If using Vue CLI 3/4, by default it will be served on localhost:8080
-  win.loadURL('http://localhost:8080')
+  win.loadURL("http://localhost:8080");
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
   }
-})
+});
 
-app.on('activate', () => {
+app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
+    createWindow();
   }
-})
+});
