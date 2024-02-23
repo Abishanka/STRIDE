2b2e9f823c3e37ce6763038d9c070062c89779da
jorsab121 <sabo.jordan121@gmail.com>
2024-02-23 18:11:06 -0500

    function for getting waterfall data from database added to electron

--- electron/main.js
+++ electron/main.js
@@ -3,8 +3,11 @@ const sqlite3 = require("sqlite3");
 
 const DB_PATH = "database/stride.db";
 
+//window var
+let win;
+
 //connect to db
-const db = new sqlite3.Database(DB_PATH, sqlite3.OPEN_READONLY, (err) => {
+const db = new sqlite3.Database(DB_PATH, sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error("Error opening database:", err.message);
   } else {
@@ -13,7 +16,7 @@ const db = new sqlite3.Database(DB_PATH, sqlite3.OPEN_READONLY, (err) => {
 });
 
 function createWindow() {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
@@ -24,6 +27,33 @@ function createWindow() {
   // Load the Vue app
   // If using Vue CLI 3/4, by default it will be served on localhost:8080
   win.loadURL("http://localhost:8080");
+}
+
+function getWaterfallData() {
+  //var to hold cadet info: uid, first_name, last_name, school
+  let cadets;
+
+  const getCadetData = new Promise((resolve, reject) => {
+    db.all(
+      "SELECT uid, first_name, last_name, school FROM CadetProfile ORDER BY school",
+      (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      }
+    );
+  });
+
+  Promise.all([getCadetData])
+    .then(([cadetRows]) => {
+      cadets = cadetRows;
+      console.log("Waterfall Data:", cadets);
+    })
+    .catch((err) => {
+      console.error("Error:", err.message);
+    });
 }
 
 app.whenReady().then(createWindow);
@@ -38,4 +68,8 @@ app.on("activate", () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
+});
+
+app.on("get-waterfall-data", () => {
+  getWaterfallData();
 });
