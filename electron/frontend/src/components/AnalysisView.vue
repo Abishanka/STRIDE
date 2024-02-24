80f44d500928709d386bc8aba3b4b31a457323bb
Shehzad Shah <64668205+shehzad02@users.noreply.github.com>
2024-02-23 21:39:14 -0500

    Fix WIP sites

--- electron/frontend/src/components/AnalysisView.vue
+++ electron/frontend/src/components/AnalysisView.vue
@@ -2,6 +2,7 @@
     <div class="analysis-view">
       <h1>Analysis</h1>
       <p>This is the Analysis page. Content will be added here later.</p>
+      <img src="https://i.imgur.com/DivezgK.png" alt="Funny Image" class="funny-image">
     </div>
   </template>
   
@@ -13,7 +14,16 @@
   
   <style scoped>
   .analysis-view {
+    color: white;
     padding: 20px;
+    text-align: center;
+    background-color: #000;
+  }
+  
+  .funny-image {
+    width: 100%;
+    max-width: 100%;
+    height: auto;
   }
   </style>
   
--- electron/frontend/src/components/BlueCardsView.vue
+++ electron/frontend/src/components/BlueCardsView.vue
@@ -2,6 +2,7 @@
     <div class="blue-cards-view">
       <h1>Blue Cards</h1>
       <p>This is the Blue Cards page. Content will be added here later.</p>
+      <img src="https://i.imgur.com/DivezgK.png" alt="Funny Image" class="funny-image">
     </div>
   </template>
   
@@ -13,7 +14,16 @@
   
   <style scoped>
   .blue-cards-view {
+    color: white; 
     padding: 20px;
+    text-align: center;
+    background-color: #000;
+  }
+  
+  .funny-image {
+    width: 100%;
+    max-width: 100%;
+    height: auto;
   }
   </style>
   
