2706ea8aaab159e3847e8b599a1f2fe06dc623eb
Abishanka <abishanka.saha@gmail.com>
2024-04-08 09:57:39 -0400

    Made some changes to cadet profile page

--- electron/frontend/src/components/CadetProfileView.vue
+++ electron/frontend/src/components/CadetProfileView.vue
@@ -17,6 +17,15 @@
         </div>
       </div>
       <div class="content-container">
+        <div class="cadet-menu-form">
+          <div class="form-row">
+            <div class="form-group">
+              <label for="cadet-menu" class="form-label">Leadership</label>
+              <div class="cadetMenu-options" v-for="option in cadetMenuOptions" :key="option"
+              :class="{ 'cadetMenu-selected-option': option === cadetMenu_option }" @click="leaderOptionClick(option)">{{ option }}</div>
+            </div>
+          </div>
+        </div>
         <div class="cadet-form">
           <h1 class="cadet-form-heading">Cadet</h1>
           <div class="form-row">
@@ -71,7 +80,8 @@ export default {
       } else {
         return [];
       }
-    })
+    }),
+    cadetMenuOptions: ['Import cadets', 'Add a cadet', 'Edit a cadet']
     }
   },
   methods: {
@@ -112,5 +122,86 @@ import '../assets/styles/Sidebar.css';
       margin: 0 auto;
       padding: 0;
       display: flex;
+  }
+
+  .content-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 20px;
+    flex-grow: 1;
+  }
+
+  .cadet-form .cadet-menu-form {
+      position: relative;
+      width: 60vw;
+      padding-top: 2.5vh;
+      background-color: #1E1E1E;
+      padding: 3vh;
+      border-radius: 5px;
+      text-align: center;
+  }
+
+  .cadet-form-heading {
+      font-family: sans-serif;
+      color: #6EA171;
+  }
+
+  .form-row {
+      display: flex;
+      justify-content: space-between;
+      flex-wrap: wrap;
+      margin: 0;
+      margin-bottom: 3vh;
+  }
+
+  .form-row input {
+      width: 100%;
+      padding: 10px;
+      margin-bottom: 20px;
+      border: none;
+      border-bottom: 1px solid #fff;
+      background-color: #1E1E1E;
+      color: #D2D1D1;
+  }
+
+  .form-row input:focus {
+      outline: none;
+      border-bottom: 1px solid #6EA171;
+  }
+
+  .form-label {
+      margin-bottom: 5px;
+      display: block;
+      text-align: left;
+      color: #D2D1D1;
+  }
+
+  .form-group {
+      margin: 0;
+  }
+
+  .form-group .bluecard-options .cadetMenu-options {
+      color: white;
+      display: inline-block;
+      align-items: center;
+      justify-content: center;
+      background-color: #191919;
+      padding: 1vh;
+      width: 5.6vw;
+      border-color: transparent;
+  }
+
+  .bluecard-options:hover {
+      background-color: #6EA171;
+      cursor: pointer;
+  }
+
+  .bluecard-options:focus {
+      outline: none;
+  }
+
+  .bluecard-selected-option {
+      background-color: #4D784E !important;
   }
 </style>
